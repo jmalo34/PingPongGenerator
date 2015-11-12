d40466e9cefe7da1c0c63e64ba3ac93243267a84
Jenna Maloney <jmalo@uw.edu>
2015-11-12 15:00:41 -0800

    make first test fail

--- src/PingPongGenerator.php
+++ src/PingPongGenerator.php
@@ -1,9 +1,32 @@
 <?php
     class PingPongGenerator
     {
-        function generatePingPongArray
+        function generatePingPongArray($input_number)
         {
-            
+            $count_up = array();
+
+            $count_from = 0;
+
+            while ($count_from < $input_number)
+            {
+                if ($count_from % 3)
+                {
+                    array_push($count_up, 'ping');
+                    $count_from = $count_from +1;
+                }
+                elseif ($count_from % 5)
+                {
+                    array_push($count_up, 'pong');
+                    $count_from = $count_from +1;
+                }
+                else
+                {
+                    array_push($count_up, $count_from);
+                    $count_from = $count_from +1;
+                }
+            }
+
+            return $count_up;
         }
     }
  ?>
--- tests/PingPongGeneratorTest.php
+++ tests/PingPongGeneratorTest.php
@@ -1,32 +1,32 @@
 <?php
     require_once "src/PingPongGenerator.php";
 
-    class PingPongGenerator extends PHPUnit_Framework_TestCase
+    class PingPongGeneratorTest extends PHPUnit_Framework_TestCase
     {
-        function test_generatePingPongArray_()
+        function test_generatePingPongArray_count()
         {
             //Arrange
-            $test_generatePingPongArray = new PingPongGenerator;
-            $input = ;
+            $test_PingPongGenerator = new PingPongGenerator;
+            $input = 5;
 
             //Act
             $result = $test_PingPongGenerator->generatePingPongArray($input);
 
             //Assert
-            $this->assertEquals(, $result);
+            $this->assertEquals(array(1, 2, 3, 4, 5), $result);
         }
-
-        function test_generatePingPongArray_()
-        {
-            //Arrange
-            $test_generatePingPongArray = new PingPongGenerator;
-            $input = ;
-
-            //Act
-            $result = $test_PingPongGenerator->generatePingPongArray($input);
-
-            //Assert
-            $this->assertEquals(, $result);
-        }
+        //
+        // function test_generatePingPongArray_Ping()
+        // {
+        //     //Arrange
+        //     $test_generatePingPongArray = new PingPongGenerator;
+        //     $input = ;
+        //
+        //     //Act
+        //     $result = $test_PingPongGenerator->generatePingPongArray($input);
+        //
+        //     //Assert
+        //     $this->assertEquals(, $result);
+        // }
     }
  ?>
